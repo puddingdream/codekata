2c311bbd9fb90ba0d8b76ec6e30245d839556f85
puddingdream <Edcrfv098@naver.com>
2026-02-12 09:48:45 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int angle) {
+        if(0 < angle && angle < 90 ) return 1;
+        if(angle == 90) return 2;
+        if (90 < angle && angle < 180 ) return 3;
+        return 4;
+    }
+}
